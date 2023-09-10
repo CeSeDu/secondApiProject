598deb9782a81faf00b3937bfaf6872b58c81fbe
Sena Durmuş <senad8127@gmail.com>
2023-09-10 20:44:54 +0300

    fixed emd
    Signed-off-by: Sena Durmuş <senad8127@gmail.com>

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ searchButton.addEventListener("click", () => {
         console.log(data[0].capital[0]);
         console.log(data[0].flags[0]);
         console.log(data[0].name.common);
+        console.log(data[0].maps.openStreetMaps);
         console.log(data[0].continents[0]);
         console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
         console.log(Object.values(data[0].languages).toString().split(",").join(", ") );
@@ -23,6 +24,7 @@ searchButton.addEventListener("click", () => {
           <div class="country__common">Name : <span class="font__light"> ${data[0].name.common} </span></div>
           <div class="country__capital"> Capital : <span class="font__light">${data[0].capital[0]}</span></div>
           <div class="country__continents">Continents:<span class="font__light"> ${data[0].continents[0]}</span></div>
+          <div  class="country__map">Map:<span class="font__light">  ${data[0].maps.openStreetMaps}</span></div>
           <div class="country__currencies">Currencies :<span class="font__light"> ${data[0].currencies[Object.keys(data[0].currencies)].name}</span></div>
           <div class="country__languages">Languages :<span class="font__light"> ${Object.values(data[0].languages).toString().split(",").join(", ")}</span></div>
           
