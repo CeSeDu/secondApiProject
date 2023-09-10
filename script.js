96ae549122c6a1bd49eecb5b0e935c798464d1b6
Sena Durmuş <senad8127@gmail.com>
2023-09-10 19:56:22 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let searchButton = document.querySelector(".search__button");
+let countryInput = document.querySelector(".search__input");
+
+searchButton.addEventListener("click", () => {
+    let countryName = countryInput.value;
+    let finalURL = `https://restcountries.com/v3/name/${countryName}?fullText=true`;
+    console.log(finalURL);
+
+    fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data[0]);
+        console.log(data[0].capital[0]);
+        console.log(data[0].flags[0]);
+        console.log(data[0].name.common);
+        console.log(data[0].continents[0]);
+        console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+        console.log(Object.values(data[0].languages).toString().split(",").join(", ") );
+
+
+        result.innerHTML = `
+          <img src="${data[0].flags[0]}" class="flag__img" >
+          <div class="country__common">Name : <span class="font__light"> ${data[0].name.common} </span></div>
+          <div class="country__capital"> Capital : <span class="font__light">${data[0].capital[0]}</span></div>
+          <div class="country__continents">Continents:<span class="font__light"> ${data[0].continents[0]}</span></div>
+          <div class="country__currencies">Currencies :<span class="font__light"> ${data[0].currencies[Object.keys(data[0].currencies)].name}</span></div>
+          <div class="country__languages">Languages :<span class="font__light"> ${Object.values(data[0].languages).toString().split(",").join(", ")}</span></div>
+          
+          `;
+
+         } );
+    });
+
+
+
